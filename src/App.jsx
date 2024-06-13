5c171838dab04b71d93f67944c590f7faa6c491e
Bruno Collazo <brunoscollazo@gmail.com>
2024-06-13 20:07:44 -0300

    Homepage básica y componente MovieCard
    Componente MovieCard muestra información de la película para usar en homepage.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,81 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { discoverMovies } from "./services/TmdbApi";
 import MovieList from "./types/MovieList";
 import Movie from "./types/Movie";
+import { Box, Grid, useMediaQuery } from "@mui/material";
+import MovieCard from "./components/MovieCard";
 
 function App() {
 	const [movies, setMovies] = useState(null);
+	const [loading, setLoading] = useState(true);
+	const [background, setBackground] = useState();
+	const isLgUp = useMediaQuery("(min-width: 1200px)");
 
-	if (movies == null) {
-		discoverMovies(1).then((data) => setMovies(data));
-	}
+	useEffect(() => {
+		setLoading(true);
+		discoverMovies(1).then((data) => {
+			setMovies(data);
+			if (data?.results?.length > 0 && data.results[0] instanceof Movie) {
+				setBackground(data.results[0].backdrop_path);
+			}
+			setLoading(false);
+		});
+	}, []);
 
 	return (
-		<ul>
-			{movies instanceof MovieList
-				? movies.results.map((movie) => {
-						if (movie instanceof Movie) {
-							return <li key={movie.id}>{movie.title}</li>;
-						}
-				  })
-				: null}
-		</ul>
+		<Box
+			sx={{
+				padding: "0",
+				backgroundColor: "rgba(0,0,0,1)",
+				zIndex: "-1",
+			}}
+		>
+			<Box
+				sx={{
+					background: background
+						? `linear-gradient(0deg, rgba(0,0,0,1) 20%, rgba(255,255,255,0) 100%),
+							url("https://image.tmdb.org/t/p/original${background}")`
+						: "inherit",
+					backgroundSize: "100vw calc(((100vw / 16) * 9) + 10px)",
+					backgroundRepeat: "no-repeat",
+					padding: "2rem",
+				}}
+			>
+				<Grid container spacing={2}>
+					{movies instanceof MovieList
+						? movies.results.map((movie) => {
+								if (movie instanceof Movie) {
+									return (
+										<Grid
+											item
+											xs={6}
+											md={3}
+											key={movie.id}
+											onMouseEnter={() =>
+												setBackground(
+													movie.backdrop_path
+												)
+											}
+											onMouseLeave={() =>
+												setBackground(
+													movies.results[0]
+														.backdrop_path
+												)
+											}
+										>
+											<MovieCard
+												movie={movie}
+												poster_size={isLgUp ? 300 : 185}
+											/>
+										</Grid>
+									);
+								}
+						  })
+						: null}
+				</Grid>
+			</Box>
+		</Box>
 	);
 }
 
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,102 @@
+import {
+	Box,
+	Card,
+	CardActionArea,
+	CardContent,
+	CardMedia,
+	Typography,
+} from "@mui/material";
+import Movie from "../types/Movie";
+
+export default function MovieCard({ movie, poster_size }) {
+	if (movie instanceof Movie) {
+		return (
+			<Card
+				sx={{
+					marginY: "10px",
+					width: `${poster_size ? poster_size : "185"}px`,
+				}}
+			>
+				<CardActionArea
+					sx={{
+						opacity: "0.9",
+						transition: "opacity .5s ease-out",
+						"&:hover": {
+							opacity: "1.0",
+						},
+					}}
+				>
+					<CardMedia
+						image={
+							"https://image.tmdb.org/t/p/w" +
+							(poster_size ? poster_size : "185") +
+							movie.poster_path
+						}
+						sx={{
+							height: `${
+								poster_size ? poster_size * (278 / 185) : "278"
+							}px`,
+						}}
+					>
+						<Box
+							sx={{
+								width: "100%",
+								height: "100%",
+								backgroundImage:
+									"linear-gradient(0deg, rgba(0,0,0,1) 10%, rgba(255,255,255,0) 100%)",
+							}}
+						></Box>
+						<CardContent
+							sx={{
+								padding: "16px",
+								position: "absolute",
+								bottom: "0",
+								width: "calc(100% - 32px)",
+								height: "calc(100% - 32px)",
+								"&:last-child": {
+									height: "calc(100% - 38px)",
+								},
+								color: "white",
+								"&:hover": {
+									"& .ellipsis": {
+										overflow: "unset",
+										textOverflow: "unset",
+										whiteSpace: "unset",
+									},
+								},
+							}}
+						>
+							<Typography
+								variant="h5"
+								className="ellipsis"
+								sx={{
+									position: "absolute",
+									bottom: "44px",
+									overflow: "hidden",
+									textOverflow: "ellipsis",
+									whiteSpace: "nowrap",
+									fontWeight: "700",
+									width: "inherit",
+								}}
+							>
+								{movie.title}
+							</Typography>
+							<Typography
+								variant="subtitle1"
+								sx={{
+									position: "absolute",
+									bottom: "16px",
+									width: "inherit",
+								}}
+							>
+								{movie.release_date.substring(0, 4)}
+							</Typography>
+						</CardContent>
+					</CardMedia>
+				</CardActionArea>
+			</Card>
+		);
+	} else {
+		throw new TypeError("La película no tiene el tipo correcto.");
+	}
+}
